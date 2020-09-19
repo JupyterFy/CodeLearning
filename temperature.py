c41ff506c07c91e9c686dc2c7c8ba3335f204166
JupyterFy <793641966@qq.com>
2020-09-19 17:16:28 +0800

    some change

--- temperature.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/usr/env python3
-fahrenheit = 0
-print("Fahrenheit Celsius")
-while fahrenheit <= 250:
-    celsius = (fahrenheit - 32) / 1.8 #switch to celsius
-    print("{:5d} {:7.2f}".format(fahrenheit, celsius))
-    fahrenheit = fahrenheit + 25
-
